a749316325ddeec7b11b121ae96f6fb83f3c650b
PuddingPudding <hwsh70119@gmail.com>
2017-06-15 22:24:37 +0800

    已完成程式主體，尚未製作測試案例

--- src/Prototype.java
+++ src/Prototype.java
@@ -10,6 +10,11 @@ public class Prototype {
 	
 	public static final int deliverDistance = 10;
 	public static final int deliverBill = 500;
+	public static final char[] menuName = {'a','b','c','d','e','f'};
+	public static final int[] menuPrice = {50 ,40 ,50 ,65 ,40 ,75 };
+	public static final String[] discountCombine = {"ab" , "cd" , "ef" , "abcdef"};
+	public static final int[] discountValue = {15 , 25 , 35 , 100};
+	
 	
 	public Prototype (int location) throws Exception
 	{
@@ -36,12 +41,12 @@ public class Prototype {
 	
 	public static void main(String args[]) throws Exception
 	{
-		try 
+		try
 		{
 		      FileReader fr=new FileReader("test.txt");
 		      BufferedReader br=new BufferedReader(fr);
 		      String line;
-		      while ((line=br.readLine()) != null) 
+		      while ((line=br.readLine()) != null)
 		      {
 		        System.out.println(line);
 		      }
@@ -53,9 +58,27 @@ public class Prototype {
 		System.out.println(Prototype.getAllLocation() );
 		Prototype p3 = new Prototype(21);
 		System.out.println(Prototype.getAllLocation() );
-		
+		System.out.println(p1.order(25, "abc") );
+		System.out.println(p2.order(25, "ccee") );
+		System.out.println(p2.order(25, "ccdd") );
 	}
 	 
+	public String order(int yourLocation , String orderList)
+	{
+		String output = "";
+		if(this.checkDeliver(yourLocation,300) == true)
+		{
+			output += "可外送 ";
+		}
+		else
+		{
+			output += "不可外送";
+		}
+		output += "\n你點的餐點為: " + orderList;
+		output += "\n一共是: " + this.orderMeal(orderList);
+		return output;
+	}
+	
 	public boolean checkDeliverByDistance(int yourLocation)
 	{
 		boolean output = true;
@@ -71,6 +94,53 @@ public class Prototype {
 		if(bill < this.deliverBill)
 		{			
 			output = false;
+		}
+		return output;
+	}
+	public boolean checkDeliver(int yourLocation , int bill)
+	{
+		return this.checkDeliverByDistance(yourLocation) || this.checkDeliverByBill(bill);
+	}
+	
+	public int orderMeal(String orderList)
+	{
+		int output = 0;
+		char temp;
+		int orderCheck; //點餐檢查器，用來檢視你點了什麼餐，他會記錄你字串中的哪個位子裡有該餐點
+		int discountNumber = 0; //用來記錄每個折扣分別出現了幾次
+		int discountNumberMin = 0;
+		char discountChar;
+		for(int i = 0 ; i < this.menuName.length ; i++)
+		{
+			orderCheck = orderList.indexOf(this.menuName[i]);
+			while(orderCheck != -1)
+			{
+				output += this.menuPrice[i];
+				orderCheck = orderList.indexOf(this.menuName[i],orderCheck+1);
+			}
+		}
+		
+		for(int i = 0 ; i < this.discountCombine.length ; i++)
+		{
+			discountNumber = 0;
+			discountNumberMin = this.discountCombine[i].length();
+			for(int j = 0 ; j < this.discountCombine[i].length() ; j++)
+			{
+				discountNumber = 0;
+				discountChar = this.discountCombine[i].charAt(j);
+				orderCheck = orderList.indexOf(discountChar);
+				if(orderCheck != -1)
+				{
+					discountNumber++;
+				}
+				while(orderCheck != -1)
+				{
+					discountNumber++;
+					orderCheck = orderList.indexOf(this.menuName[i],orderCheck+1);
+				}
+				discountNumberMin = Math.min(discountNumber, discountNumberMin);
+			}
+			output -= this.discountValue[i]*discountNumberMin;
 		}
 		return output;
 	}
